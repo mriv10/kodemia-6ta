18d6d9401d2315d21847a01603fadb6678aedde5
mriv10 <58017773+mriv10@users.noreply.github.com>
2019-12-03 00:23:08 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('Hello Koders!')
+var MexicanPeso
+var Euro
+var Dollar
+var CubanPeso
+var CubaNationalPeso
+
+MexicanPeso=500
+//tasa de cambio 1 Euro = 21.66 Pesos Mexicanos.
+Euro=MexicanPeso/21.66
+console.log('Son '+ Euro + ' Euro')
+//tasa de cambio 1 Dólar = 19.56 Pesos Mexicanos. 
+Dollar=MexicanPeso/19.56
+console.log('Son '+ Dollar + ' USD')
+//tasa de cambio 1 Peso Cubano = 19.69 Pesos Mexicanos.
+CubaNationalPeso=MexicanPeso/19.69
+//tasa de cambio 1 Peso Cubano = 0.038 Peso Cubano Convertible.
+CubanPeso=CubaNationalPeso*0.038
+console.log('Son '+ CubanPeso + ' CUC')
